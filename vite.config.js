f8e85f9d525744ea9a143027ec0f60f4e855d969
Oanh Phạm <opham414@gmail.com>
2026-01-05 21:32:21 +0700

    build for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  base: '/sentiment-analysis-client-fe/',
 })
